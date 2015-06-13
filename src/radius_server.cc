86f83cb155147a38404a878aa87b2aaa098dc058
Michał Nowotnik <m.nowot@gmail.com>
2015-06-13 12:41:00 +0200

    server logic added

--- src/radius_server.cc
+++ src/radius_server.cc
@@ -7,8 +7,16 @@ using radius::packets::RadiusPacket;
 using radius::packets::EapPacket;
 using radius::packets::EapIdentity;
 using radius::packets::EapData;
+using radius::packets::EapMd5Challenge;
+using radius::packets::RadiusAVP;
+using radius::packets::EapMessage;
 
 namespace radius {
+
+    namespace{
+        const int MIN_CHAL_VAL=12;
+        const int MAX_CHAL_VAL=24;
+    }
 
 RadiusServer::RadiusServer(const map<string, string> &userPassMap,
                            const string &secret, const Logger &logger)
@@ -52,8 +60,31 @@ const vector<Packet> RadiusServer::recvPacket(const Packet &packet) {
             std::string userName = eapIden->getIdentity();
             const auto passPtr = userPassMap.find(userName);
             if (passPtr == userPassMap.end()) {
+                logger->trace() << "Username " + userName + " not found";
                 return addPendingPackets(packetsToSend);
             }
+
+            const std::string userPass = passPtr->second;
+            EapPacket eapReq;
+            eapReq.setType(EapPacket::REQUEST);
+            eapReq.setIdentifier(eapPacket.getIdentifier()+1);
+
+            std::vector<byte>challenge = generateRandomBytes(MIN_CHAL_VAL/4,MAX_CHAL_VAL/4);
+            EapMd5Challenge md5Chal;
+            md5Chal.setValue(challenge);
+            eapReq.setData(dynamic_cast<const EapData&>(md5Chal));
+            EapMessage eapM;
+            eapM.setValue(eapReq.getBuffer());
+
+            RadiusPacket sendPacket;
+            sendPacket.setCode(RadiusPacket::ACCESS_CHALLENGE);
+            sendPacket.setIdentifier(radiusPacket.getIdentifier()+1);
+            sendPacket.addAVP(dynamic_cast<const RadiusAVP&>(eapM));
+            sendPacket.setAuthenticator(radiusPacket.getAuthenticator());
+            calcAndSetMsgAuth(sendPacket,secret);
+            calcAndSetAuth(sendPacket);
+            Packet packetToSend(sendPacket.getBuffer(),packet.addr);
+            packetsToSend.push_back(packetToSend);
         }
     } catch (const packets::InvalidPacket &e) {
         logger->error() << "Packet invalid. Reason :" << e.what();
